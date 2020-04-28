d6f91b02ee70e63f7d1f0f61e93e13b3843fae3b
David.OA <david.oconte@orange.fr>
2020-04-28 13:57:05 +0200

    Is the first part of the tutos.

--- WorkManager/app/src/main/java/com/oconte/david/workmanager/MainActivity.java
+++ WorkManager/app/src/main/java/com/oconte/david/workmanager/MainActivity.java
@@ -2,6 +2,10 @@ package com.oconte.david.workmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //This is the subclass of our WorkRequest
+        final OneTimeWorkRequest workRequest = new OneTimeWorkRequest.Builder(MyWorker.class).build();
+
+        //A click listener for the button
+        //inside the onClick method we will perform the work
+        findViewById(R.id.buttonEnqueue).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Enqueuing the work request
+                WorkManager.getInstance().enqueue(workRequest);
+            }
+        });
     }
 }
