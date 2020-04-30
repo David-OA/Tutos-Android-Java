06907c7b834856c0befcfa7c89948278d30519aa
David.OA <david.oconte@orange.fr>
2020-04-30 12:07:20 +0200

    I add some options for WorkManager.

--- WorkManager/app/src/main/java/com/oconte/david/workmanager/MainActivity.java
+++ WorkManager/app/src/main/java/com/oconte/david/workmanager/MainActivity.java
@@ -1,14 +1,20 @@
 package com.oconte.david.workmanager;
 
 import android.arch.lifecycle.Observer;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.concurrent.TimeUnit;
+
+import androidx.work.Constraints;
 import androidx.work.Data;
 import androidx.work.OneTimeWorkRequest;
+import androidx.work.PeriodicWorkRequest;
 import androidx.work.WorkManager;
 import androidx.work.WorkStatus;
 
@@ -16,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mTextView;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,15 +36,30 @@ public class MainActivity extends AppCompatActivity {
                 .putString(MyWorker.EXTRA_TEXT, "Hi! I have come from activity.")
                 .build();
 
+        // Adding Constraints
+        /*Constraints constraints = new Constraints.Builder()
+                .setRequiresCharging(true)
+                .setRequiresDeviceIdle(true)
+                .build();
+        */
+
+        /*final PeriodicWorkRequest periodicWorkRequest = new PeriodicWorkRequest.Builder(MyWorker.class, 12, TimeUnit.HOURS)
+                .addTag("periodic_work")
+                .build();
+        */
+
         //This is the subclass of our WorkRequest
         final OneTimeWorkRequest simpleRequest = new OneTimeWorkRequest.Builder(MyWorker.class)
                 .setInputData(data)
+                //.setConstraints(constraints)
                 .build();
 
         findViewById(R.id.simpleWorkButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 WorkManager.getInstance().enqueue(simpleRequest);
+                //WorkManager.getInstance().enqueue(periodicWorkRequest);
+
             }
         });
 
@@ -56,5 +78,7 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 });
+
+
     }
 }
