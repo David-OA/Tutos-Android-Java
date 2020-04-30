3582f07a454d621607259a468f5abe5903eb906e
David.OA <david.oconte@orange.fr>
2020-04-30 14:49:56 +0200

    I finished the easy parts for this tutos.

--- Swipe/app/src/main/java/com/oconte/david/swipe/MainActivity.java
+++ Swipe/app/src/main/java/com/oconte/david/swipe/MainActivity.java
@@ -1,13 +1,39 @@
 package com.oconte.david.swipe;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tvSwipDescription;
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeView();
+        tvSwipDescription.setOnTouchListener(new OnSwipeTouchListener(MainActivity.this) {
+            public void onSwipeTop() {
+                Toast.makeText(getApplicationContext(), getResources().getString(R.string.toastTop), Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeRight() {
+                Toast.makeText(getApplicationContext(), getResources().getString(R.string.toastRight), Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeLeft() {
+                Toast.makeText(getApplicationContext(), getResources().getString(R.string.toastLeft), Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeBottom() {
+                Toast.makeText(getApplicationContext(), getResources().getString(R.string.toastBottom), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void initializeView() {
+        tvSwipDescription=(TextView) findViewById(R.id.tvSwipDescription);
     }
 }
