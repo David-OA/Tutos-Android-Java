d5a4260281c27ad0732a52abfd8a6c433dbff4c0
David.OA <david.oconte@orange.fr>
2020-05-05 15:47:03 +0200

    I try a more easier tutorial.

--- WorkManager2/app/src/main/java/com/example/workmanager2/NotificationHandler.java
+++ WorkManager2/app/src/main/java/com/example/workmanager2/NotificationHandler.java
@@ -1,4 +1,46 @@
 package com.example.workmanager2;
 
-public class NotificationHandler {
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.UserDictionary;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class NotificationHandler extends Worker {
+
+    public final String CHANNEL_ID = "12";
+    public final int notificatioId = 1;
+    public NotificationHandler(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+    @NonNull
+    @Override
+    public Result doWork() {
+//our work
+        createNofication();
+//indicate whether work was successful
+        return Result.success();
+    }
+    public void createNofication(){
+//intent to open our activity
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(),0,intent,0);
+//notifications
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(),CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_alarm_white)
+                .setContentTitle("Event Reminder")
+                .setContentText("Hello, 4 days remaining to event xyz")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+//show notification
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(getApplicationContext());
+        notificationManagerCompat.notify(notificatioId,builder.build());
+    }
 }
