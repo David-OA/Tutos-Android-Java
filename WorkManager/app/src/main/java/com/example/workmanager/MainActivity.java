5ba8b1f1e19afa3f09a81b80eb3aa2ec0bad734d
David.OA <david.oconte@orange.fr>
2020-05-07 14:24:02 +0200

    I add OneTimeWorkRequest when onClick Button..

--- WorkManager/app/src/main/java/com/example/workmanager/MainActivity.java
+++ WorkManager/app/src/main/java/com/example/workmanager/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.workmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //This is the subclass of our WorkRequest
+        final OneTimeWorkRequest workRequest = new OneTimeWorkRequest.Builder(MyWorker.class).build();
+
+        //A click listener for the button
+        //inside the onClick method we will perform the work
+        findViewById(R.id.buttonEnqueue).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Enqueuing the work request
+                WorkManager.getInstance().enqueue(workRequest);
+            }
+        });
     }
 }
