f1635935e0e35e094518b521c4f00955ba6066a2
David.OA <david.oconte@orange.fr>
2020-05-16 15:13:07 +0200

    I add the first parts for ListView.

--- ListView/app/src/main/java/com/oconte/david/listview/MainActivity.java
+++ ListView/app/src/main/java/com/oconte/david/listview/MainActivity.java
@@ -2,12 +2,33 @@ package com.oconte.david.listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    private String[] prenoms = new String[]{
+            "Antoine", "Benoit", "Cyril", "David", "Eloise", "Florent",
+            "Gerard", "Hugo", "Ingrid", "Jonathan", "Kevin", "Logan",
+            "Mathieu", "Noemie", "Olivia", "Philippe", "Quentin", "Romain",
+            "Sophie", "Tristan", "Ulric", "Vincent", "Willy", "Xavier",
+            "Yann", "Zoé"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        //android.R.layout.simple_list_item_1 est une vue disponible de base dans le SDK android,
+        //Contenant une TextView avec comme identifiant "@android:id/text1"
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, prenoms);
+        listView.setAdapter(adapter);
     }
 }
