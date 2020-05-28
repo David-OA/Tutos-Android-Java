e937f361c0f52f4d30a8c9614e00a97d310ddfba
David.OA <david.oconte@orange.fr>
2020-05-28 15:42:39 +0200

    I add Interface, Model and Common.

--- HighlightRecyclerview/app/src/main/java/com/example/highlightrecyclerview/MainActivity.java
+++ HighlightRecyclerview/app/src/main/java/com/example/highlightrecyclerview/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.highlightrecyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.highlightrecyclerview.Model.Item;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView list_item;
+    RecyclerView.LayoutManager layoutManager;
+    List<Item> items;
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fab = (FloatingActionButton)findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, NewActivity.class));
+            }
+        });
+
+        list_item = (RecyclerView)findViewById(R.id.recyclerView);
+        layoutManager = new LinearLayoutManager(this);
+        list_item.setHasFixedSize(true);
+        list_item.setLayoutManager(layoutManager);
+        
+        getData();
+    }
+
+    private void getData() {
+        //Generate data
+        for (int i = 0; i<10; i++)
+        {
+            Item item = new Item();
+            item.setName("Item" +i);
+            if (i%2 == 0)
+                item.setChecked(true);
+            else
+                item.setChecked(false);
+            items.add(item);
+        }
     }
 }
