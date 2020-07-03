b33d3cdb3e38b8489c8b69cab3202e2d46c3e49a
David.OA <david.oconte@orange.fr>
2020-07-03 15:44:27 +0200

    It's a tutorial for use Espresso.

--- Espresso/app/src/androidTest/java/com/example/espresso/ExampleInstrumentedTest.java
+++ Espresso/app/src/androidTest/java/com/example/espresso/ExampleInstrumentedTest.java
@@ -1,27 +1,89 @@
 package com.example.espresso;
 
-import android.content.Context;
-
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
-
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.espresso", appContext.getPackageName());
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.filters.LargeTest;
+import androidx.test.runner.AndroidJUnit4;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.core.AllOf.allOf;
+
+
+@LargeTest
+public class ExampleInstrumentedTest {
+
+    @Rule
+    public final ActivityScenarioRule<MainActivity> mainActivityActivityTestRule = new ActivityScenarioRule<MainActivity>(MainActivity.class);
+
+    @Before
+    public void setup() throws Exception{
+        //this.context = InstrumentationRegistry.getTargetContext();
+    }
+
+    /*on précise que l'on veut tester un MainActivity
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+
+        //doit être appelé dans le setup
+        getActivity();
+    }*/
+
+    @Test
+    public void testContainsIntialViews() {
+        //je vais tester ici que l'EditText et le bouton LOGIN sont bien affichés
+        //mais que le TextView "Hello XXXX" n'est pas présent
+
+        onView(withId(R.id.editText)).check(matches(isDisplayed()));
+        onView(withText("LOGIN")).check(matches(isDisplayed()));
+        onView(withId(R.id.text)).check(matches(not(isDisplayed())));
+    }
+
+    //teste le comportement de l'écran si on appuie sur LOGIN sans avoir écrit de nom
+    @Test
+    public void testClickLogin_emptyText() {
+        //je vais cliquer sur LOGIN, mais sans avoir écrit de texte dans l'EditText
+        onView(withText("LOGIN")).perform(click());
+
+        //Ce qui ne devrait pas cacher le bouton LOGIN, ni afficher le "Hello XXXX"
+        onView(withText("LOGIN")).check(matches(isDisplayed()));
+        onView(withId(R.id.text)).check(matches(not(isDisplayed())));
+    }
+
+    @Test
+    public void testClickLogin_withText() {
+        //je vais écrire "florent" dans l'EditText
+        onView(withId(R.id.editText)).perform(typeText("florent"));
+
+        //puis clicker sur le bouton LOGIN
+        onView(withText("LOGIN")).perform(click());
+
+        //ce qui devrait faire disparaitre LOGIN et l'EditText
+        onView(withText("LOGIN")).check(matches(not(isDisplayed())));
+        onView(withId(R.id.editText)).check(matches(not(isDisplayed())));
+
+        //puis afficher "Hello florent"
+        onView(withId(R.id.text)).check(matches(allOf(isDisplayed(), withText("Hello florent"))));
     }
 }
--- Espresso/app/src/main/java/com/example/espresso/MainActivity.java
+++ Espresso/app/src/main/java/com/example/espresso/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.espresso;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.login) Button login;
+    @BindView(R.id.editText) EditText editText;
+    @BindView(R.id.loginContainer) View loginContainer;
+    @BindView(R.id.textContainer) View textContainer;
+    @BindView(R.id.text) TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    @OnClick(R.id.login)
+    public void onLoginClicked(){
+        String name = editText.getText().toString();
+        if(!name.isEmpty()) {
+            closeKeyboard(editText);
+
+            //hide views
+            loginContainer.setVisibility(View.GONE);
+            textContainer.setVisibility(View.VISIBLE);
+
+            //display
+            text.setText("Hello " + name);
+        }
+    }
+
+    private void closeKeyboard(View view){
+        //close keyboard
+        InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(AppCompatActivity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
