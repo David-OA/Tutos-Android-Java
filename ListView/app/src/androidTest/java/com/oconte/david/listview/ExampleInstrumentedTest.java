ac00efc7f85238389e04d8ac1d61de7ecfd8e658
DAVID-OA <david.oconte@orange.fr>
2020-10-14 14:34:14 +0200

    I add some test for test.

--- ListView/app/src/androidTest/java/com/oconte/david/listview/ExampleInstrumentedTest.java
+++ ListView/app/src/androidTest/java/com/oconte/david/listview/ExampleInstrumentedTest.java
@@ -1,12 +1,19 @@
 package com.oconte.david.listview;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -16,11 +23,17 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<MainActivity>(MainActivity.class, false, false);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.oconte.david.listview", appContext.getPackageName());
+        //Start the MainActivity
+        mActivityRule.launchActivity(null);
+
+        onView(withId(R.id.listView)).check(matches(isDisplayed()));
     }
 }
--- ListView/app/src/main/java/com/oconte/david/listview/MainActivity.java
+++ ListView/app/src/main/java/com/oconte/david/listview/MainActivity.java
@@ -1,7 +1,7 @@
 package com.oconte.david.listview;
 
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
