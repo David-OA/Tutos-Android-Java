13b3af8bbec574497d680a55b70249286f3fece8
DAVID-OOA <daoconteid@gmail.com>
2021-09-25 17:44:07 +0200

    First commit

--- subMenu/app/src/main/java/com/oconte/david/submenu/MainActivity.java
+++ subMenu/app/src/main/java/com/oconte/david/submenu/MainActivity.java
@@ -1,19 +1,42 @@
 package com.oconte.david.submenu;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+import com.oconte.david.submenu.databinding.ActivityMainBinding;
+
+import java.util.Objects;
+
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        this.configureDrawerLayout();
+
+        this.configureNavigationView();
+
+        this.configureToolbar();
+
     }
 
     //Méthode qui se déclenchera lorsque vous appuierez sur le bouton menu du téléphone
@@ -25,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.menu, menu);
 
         //Il n'est pas possible de modifier l'icône d'en-tête du sous-menu via le fichier XML on le fait donc en JAVA
-        //menu.getItem(0).getSubMenu().setHeaderIcon(R.drawable.option_white);
+        menu.getItem(0).getSubMenu().setHeaderIcon(R.drawable.ic_launcher_foreground);
 
         return true;
     }
@@ -49,4 +72,68 @@ public class MainActivity extends AppCompatActivity {
                 return true;
         }
         return false;}
+
+
+
+        /////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Override
+    public void onBackPressed() {
+        // 5 - Handle back click to close menu
+        if (this.binding.activityMainDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.binding.activityMainDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    /**
+     * - Configure Drawer Layout
+     */
+    private void configureDrawerLayout() {
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, binding.activityMainDrawerLayout, binding.toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        binding.activityMainDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    /**
+     * - Configure NavigationView
+     */
+    private void configureNavigationView() {
+        binding.activityMainNavView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // 4 - Handle Navigation Item Click
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.activity_main_drawer_lunch:
+                Toast.makeText(MainActivity.this, "Option", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.activity_main_drawer_settings:
+                Toast.makeText(MainActivity.this, "Option", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.activity_main_drawer_galop3:
+                //this.showFragment(FRAGMENT_LOGOUT);
+                break;
+            default:
+                break;
+        }
+
+        this.binding.activityMainDrawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+    /**
+     * - Configure the Toolbar
+     */
+    protected void configureToolbar() {
+        setSupportActionBar(binding.toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setTitle("SubMenu");
+    }
 }
