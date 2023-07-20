7a5bbfc253eb41fc47386166affaef1f3e19b54c
Oconte David <david.oconte@orange.fr>
2023-07-20 16:11:05 +0200

    I update the code.

--- WebView/app/src/main/java/com/oconte/david/webview/MainActivity.java
+++ WebView/app/src/main/java/com/oconte/david/webview/MainActivity.java
@@ -1,16 +1,19 @@
 package com.oconte.david.webview;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,10 +26,9 @@ public class MainActivity extends AppCompatActivity {
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
-
         webView.setWebViewClient(new WebViewClient() {
             @Override
-            // Fonction qui permet l'affichage de la page lorsque tout est chargé (événement onPageFinished)
+            //Function that allows the page to be displayed when everything is loaded (onPageFinished event)
 
             public void onPageFinished(WebView view, String url) {
                 findViewById(R.id.webview).setVisibility(View.VISIBLE);
